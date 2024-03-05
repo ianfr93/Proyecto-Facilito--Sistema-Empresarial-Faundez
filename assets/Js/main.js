df83e02afd4f9e66c62ab2554d949710eaa2c084
ianfr93 <albitox10@gmail.com>
2024-03-05 12:48:17 -0300

    canbios

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,41 +1,21 @@
+function validateForm() {
+  var nombre = document.getElementById('nombre').value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirmPassword').value;
+  var email = document.getElementById('email').value;
+  var confirmEmail = document.getElementById('confirmEmail').value;
 
-  let usuarios = [
-    { usuario: "usuario1", contraseña: "contraseña1" },
-    { usuario: "usuario2", contraseña: "contraseña2" },
-    { usuario: "usuario3", contraseña: "contraseña3" },
-    { usuario: "usuario4", contraseña: "contraseña4" },
-    { usuario: "usuario5", contraseña: "contraseña5" },
-    { usuario: "usuario6", contraseña: "contraseña6" }
-  ];
-
-  let intentosRestantes = 4;
-
-  // Función para autenticar al usuario
-  function autenticarUsuario(event) {
-    event.preventDefault();
-
-    let nombreUsuario = document.getElementById("username").value;
-    let contraseña = document.getElementById("password").value;
-
-    let usuarioValido = usuarios.find(user => user.usuario === nombreUsuario && user.contraseña === contraseña);
-
-    let mensajeElement = document.getElementById("mensaje");
-
-    if (!usuarioValido) {
-      intentosRestantes--;
-
-      if (intentosRestantes > 0) {
-        mensajeElement.innerHTML = `Nombre de usuario o contraseña incorrectos. Intentos restantes: ${intentosRestantes}`;
-        mensajeElement.className = "error-message";
-        mensajeElement.style.display = "block";
-      } else {
-        mensajeElement.innerHTML = "¡Se han agotado los intentos! Reinicie la sesión para intentar nuevamente.";
-        resetForm();
-      }
-    } else {
-      window.location.href = './pages/Menu-de-caja.html';
-    }
+  if (!nombre || !username || !password || !confirmPassword || !email || !confirmEmail) {
+      alert('Por favor, completa todos los campos.');
+      return;
   }
+
+  // Aquí puedes agregar más validaciones según tus requisitos
+
+  alert('Registro exitoso!');
+  // Aquí podrías agregar la lógica para enviar los datos al servidor o hacer otras acciones
+}
 
   // Agregar evento de clic al botón con id "acceso"
 document.getElementById("acceso").addEventListener("click", function(event) {
